aa46bf41b9ba2f3553e9e3f0c4fe605ab2e742ef
igorwessel <igor_wessel@hotmail.com>
2020-04-17 21:50:40 -0300

    feat: Finished component Home.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Container } from "./styled";
+import { Container, Header, Title, Input, InputContainer } from "./styled";
+import { MdSearch } from 'react-icons/md'
 
 const App = () => {
   return (
     <Container>
-      <h1>Oi</h1>
+        <Header>
+          <Title>Previsão do tempo</Title>
+          <InputContainer>
+            <Input placeholder="Insira aqui o nome da cidade"/>
+            <MdSearch size={32} style={{ position: "absolute", right: "8%", top: "30px"}}/>
+          </InputContainer>
+        </Header>
     </Container>
   );
 };
--- src/styled.js
+++ src/styled.js
@@ -1,5 +1,38 @@
 import styled from "styled-components";
+import { fontcolors } from './styles/colors'
+
+
 
 export const Container = styled.div`
+`;
+
+export const Header = styled.header`
   display: flex;
+  flex-direction: column;
+  color: ${fontcolors.primary};
+  border-bottom: 1px solid white;
+  margin: 0 30px;
+`
+
+export const Title = styled.h1`
+  font-size: 3.8em;
+  color: white;
+`;
+
+export const Input = styled.input`
+  padding: 15px;
+  width: 100%;
+  max-width: 450px;
+  margin-top: 20px;
+  margin-bottom: 70px;
+  outline: 0;
+  color: ${fontcolors.primary};
+`
+
+export const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
 `;
